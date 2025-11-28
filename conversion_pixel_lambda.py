45b2139a5b67aff5594f0e30c04ad5bb1e0e9972
EmileGauthier <154472479+EmileGauthier@users.noreply.github.com>
2025-11-28 12:18:11 -0500

    Revise pixel calibration and uncertainty calculations
    Updated calibration data and added weighted uncertainty calculations for pixel to wavelength conversion.

--- conversion_pixel_lambda.py
+++ conversion_pixel_lambda.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Données de calibration (obtenues avec des lasers de longueurs d'onde connues)
-pixel_means = np.array([562,770,863,1026])
-uncertainties = np.array([12.83798508, 15.2689612, 22.58015907,21.15126552])
-wavelengths = np.array([432,571,593,650])
+pixel_means = np.array([343,1762,2415,2708,3324])
+uncertainties = np.array([29.70196341, 188.83664198, 124.30615492,159.96283316,83.97334868])
+wavelengths = np.array([405,532,571,593,650])
 
-#plt.scatter(wavelengths,pixel_means, marker='D')
-#plt.plot(wavelengths,pixel_means)
-#plt.show()
+plt.plot(pixel_means,wavelengths)
+plt.scatter(pixel_means,wavelengths)
+plt.show()
 
 # Construction du modèle linéaire
 
@@ -18,6 +18,20 @@ m = beta[0]
 c = beta[1]
 
 # Calcul des incertitudes sur la pente et l'ordonnée à l'origine
+
+def incertitudes_poids(x, y, alpha_x, alpha_y, slope):
+    # Retourne l'incertitude sur la pente et l'ordonnée à l'origine « à la Ménard »
+
+    alpha_y_tot = np.sqrt((alpha_y **2 + (alpha_x * slope)**2))
+
+    weights = np.divide(1,alpha_y_tot**2)
+
+    Delta_prime = np.sum(weights)*np.sum(weights * x**2) - (np.sum(weights*x))**2
+
+    alpha_c = np.sqrt(np.sum(weights*x**2)/Delta_prime)
+    alpha_m = np.sqrt(np.sum(weights)/Delta_prime)
+
+    return [alpha_m, alpha_c]
 
 def incertitudes(x, y, slope, intercept):
     # Retourne l'incertitude sur la pente et l'ordonnée à l'origine « à la Ménard »
@@ -32,16 +46,25 @@ def incertitudes(x, y, slope, intercept):
 
     return [alpha_m, alpha_c]
 
+# Ajout en quadrature de l'incertitude sur x et y
+
 [alpha_m, alpha_c] = incertitudes(pixel_means, wavelengths, m, c)
+
+
+[alpha_m, alpha_c] = incertitudes_poids(pixel_means, wavelengths, 0.5, uncertainties, m)
+
 
 ### Conversion de pixels (sans unité) à longueur d'onde (nm), avec incertitudes.
 
-pixel_value = 600
+pixel_value = np.linspace(0,4000,100)
 
 wavelength_value = m * pixel_value + c
-wavelength_uncertainty = alpha_m * pixel_value + alpha_c
+wavelength_uncertainty = np.sqrt((pixel_value * alpha_m)**2 + alpha_c**2 )
 
-print(f"Pour le pixel {pixel_value}, la valeur de longueur d'onde assosciée est ({wavelength_value:.2f} ± {wavelength_uncertainty:.2f})nm")
+plt.plot(wavelength_value, wavelength_uncertainty)
+plt.show()
+
+#print(f"Pour le pixel {pixel_value}, la valeur de longueur d'onde assosciée est ({wavelength_value:.2f} ± {wavelength_uncertainty:.2f})nm")
 
 
 
