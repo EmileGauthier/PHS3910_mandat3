9ff1e4e00945b0fc410665983f28faf8fad8f50c
EmileGauthier <154472479+EmileGauthier@users.noreply.github.com>
2025-12-01 09:08:30 -0500

    Update calibration data and enhance plotting
    Updated calibration data and added error bars to the plot. Adjusted pixel range for conversion and included a print statement for wavelength output.

--- conversion_pixel_lambda.py
+++ conversion_pixel_lambda.py
@@ -2,13 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Données de calibration (obtenues avec des lasers de longueurs d'onde connues)
-pixel_means = np.array([343,1762,2415,2708,3324])
-uncertainties = np.array([29.70196341, 188.83664198, 124.30615492,159.96283316,83.97334868])
-wavelengths = np.array([405,532,571,593,650])
+pixel_means = np.array([482,2109,2375,3316])
+uncertainties = np.array([53.29419573,42.13969144,43.60817607,75.58946782])
+wavelengths = np.array([405,555,571,650])
 
-plt.plot(pixel_means,wavelengths)
-plt.scatter(pixel_means,wavelengths)
-plt.show()
 
 # Construction du modèle linéaire
 
@@ -33,19 +30,40 @@ def incertitudes_poids(x, y, alpha_x, alpha_y, slope):
 
     return [alpha_m, alpha_c]
 
-[alpha_m, alpha_c] = incertitudes_poids(pixel_means, wavelengths, 0.5, uncertainties, m)
+
+# Ajout en quadrature de l'incertitude sur x et y
+
+[alpha_m, alpha_c] = incertitudes_poids(pixel_means, wavelengths, 0, uncertainties, m)
+
+pixel_value = np.linspace(100,4000,100)
+
+plt.errorbar(pixel_means, wavelengths, xerr = 0, yerr=uncertainties, fmt='o', capsize=5, label = "Mesures expérimentales")
+plt.plot(pixel_value, pixel_value*m + c, label = "Régression linéaire")
+plt.xlabel(r"Position en $x$ sur le détecteur (pixel)", fontsize = 12)
+plt.ylabel("Longueur d'onde (nm)", fontsize = 12)
+plt.legend(fontsize = 12)
+plt.tight_layout()
+plt.show()
 
 
 ### Conversion de pixels (sans unité) à longueur d'onde (nm), avec incertitudes.
 
-pixel_value = np.linspace(0,4000,100)
+#pixel_value = np.linspace(0,4000,100)
+pixel_value = 1300
 
 wavelength_value = m * pixel_value + c
-wavelength_uncertainty = np.sqrt((pixel_value * alpha_m)**2 + alpha_c**2 )
+wavelength_uncertainty = np.sqrt((pixel_value * alpha_m)**2 + alpha_c**2)
 
-plt.plot(wavelength_value, wavelength_uncertainty)
-plt.show()
+#plt.plot(wavelength_value, wavelength_uncertainty)
+#plt.show()
 
-#print(f"Pour le pixel {pixel_value}, la valeur de longueur d'onde assosciée est ({wavelength_value:.2f} ± {wavelength_uncertainty:.2f})nm")
+print(f"Pour le pixel {pixel_value}, la valeur de longueur d'onde assosciée est ({wavelength_value:.2f} ± {wavelength_uncertainty:.2f})nm")
+
+
+
+
+
+
+
 
 
